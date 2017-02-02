e59a28bfdec60d5b7fe0c1dbe4a66209885a7074
Gustavo Victor <gustavo.cavalcante@ccc.ufcg.edu.br>
2017-02-02 11:43:07 -0300

    Adicione a Classe Animal e Estadia

--- src/hotel/Animal.java
+++ src/hotel/Animal.java
@@ -2,4 +2,27 @@ package hotel;
 
 public class Animal {
 
+	private String nome;
+	private String tipo;
+	private int idade;
+	
+	public Animal(String nome,String tipo,int idade){
+		this.nome = nome;
+		this.tipo = tipo;
+		this.idade = idade;
+		
+	}
+	
+	public string getNome(){
+		return this.nome;
+	}
+	
+	public string getTipo(){
+		return this.tipo;
+	}
+	
+	public int getIdade(){
+		return this.idade;
+	}
+	
 }
